3968392f480acdff0f4aa01c1789200c5a23e7d0
D <igothippie@yahoo.com>
2024-07-31 19:14:07 -0400

    Shock Wave math change

--- data/skills/PoD/druid.js
+++ data/skills/PoD/druid.js
@@ -46,6 +46,7 @@ var character_druid = {class_name:"Druid", strength:15, dexterity:20, vitality:2
 		if (skill.name == "Werebear" && elem == 0) { if (skills[12].level > 0) { result = (15 + skills[12].data.values[1][skills[12].level+skills[12].extra_levels]) } else { result = 15 } }
 		if (skill.name == "Werebear" && elem == 1) { if (skills[12].level > 0) { result += (skills[12].data.values[0][skills[12].level+skills[12].extra_levels]) } }
 		if (skill.name == "Fire Claws" && elem < 2) {							 result *= ((1 + (0.12*skills[0].level + 0.12*skills[1].level + 0.12*skills[7].level + 0.12*skills[9].level)) * (1+character.fDamage/100)) }
+//		if (skill.name == "Shock Wave" && elem < 2) {							 result *= (1 + ((0.08*skills[15].level)) * (1+character.physicalDamage/100)) + ((0.08*skills[15].level) * (1+character.damage_bonus/100))} // * (1+character.damage_bonus/100)} //tried to get shockwave damage right, this did not do it
 		if (skill.name == "Shock Wave" && elem < 2) {							 result *= (1 + (0.08*skills[15].level)) * (1+character.physicalDamage/100)}
 		if (skill.name == "Rabies" && elem > 0 && elem < 3) {					 result *= ((1 + (0.20*skills[22].level + 0.20*skills[27].level)) * (1+character.pDamage/100)) }
 
@@ -137,7 +138,7 @@ var character_druid = {class_name:"Druid", strength:15, dexterity:20, vitality:2
 		var skillMin = 0; var skillMax = 0; var skillAr = 0;
 		var attack = 0;	// 0 = no basic damage, 1 = includes basic attack damage
 		var spell = 2;	// 0 = uses attack rating, 1 = no attack rating, 2 = non-damaging
-		var damage_enhanced = character.damage_bonus + character.e_damage;
+		var damage_enhanced = character.damage_bonus //+ character.e_damage; //Commented this out to stop it from applying on-weapon ED to shockwave, needs more testing on other skills to make sure they dont break
 		
 		if (skill.name == "Firestorm") { 				attack = 0; spell = 1; fDamage_min = character.getSkillData(skill,lvl,1); fDamage_max = character.getSkillData(skill,lvl,2); }
 		else if (skill.name == "Molten Boulder") { 		attack = 0; spell = 1; damage_min = character.getSkillData(skill,lvl,0); damage_max = character.getSkillData(skill,lvl,1); fDamage_min = character.getSkillData(skill,lvl,2); fDamage_max = character.getSkillData(skill,lvl,3); }
@@ -175,14 +176,15 @@ var character_druid = {class_name:"Druid", strength:15, dexterity:20, vitality:2
 		if (attack == 0) { phys_min = 0; phys_max = 0; phys_mult = 1; nonPhys_min = 0; nonPhys_max = 0; damage_enhanced = 0; }
 		nonPhys_min += (fDamage_min + cDamage_min + lDamage_min + pDamage_min + mDamage_min);
 		nonPhys_max += (fDamage_max + cDamage_max + lDamage_max + pDamage_max + mDamage_max);
-//		if (skill.name == "Shock Wave") {phys_min = (~~phys_min * (phys_mult + (.25*damage_bonus)) * (1 + (weapon_damage-100)/100) + (damage_min * (1+((.25*damage_bonus)+damage_enhanced)/100)));
-//										phys_max = (~~phys_max * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_max * (1+(damage_bonus+damage_enhanced+(character.level*character.e_max_damage_per_level))/100)));
-//		}
-//		else { 	phys_min = (~~phys_min * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_min * (1+(damage_bonus+damage_enhanced)/100)));
-//				phys_max = (~~phys_max * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_max * (1+(damage_bonus+damage_enhanced+(character.level*character.e_max_damage_per_level))/100)));
-//		}
-		phys_min = (~~phys_min * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_min * (1+(damage_bonus+damage_enhanced)/100)));
-		phys_max = (~~phys_max * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_max * (1+(damage_bonus+damage_enhanced+(character.level*character.e_max_damage_per_level))/100)));
+		
+		if (skill.name == "Shock Wave") {phys_min = (~~phys_min * (phys_mult + (.25*damage_bonus)) * (1 + (weapon_damage-100)/100) + (damage_min * (1+((.25*damage_bonus)+damage_enhanced)/100)));
+										phys_max = (~~phys_max * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_max * (1+(damage_bonus+damage_enhanced+(character.level*character.e_max_damage_per_level))/100)));
+		}
+		else { 	phys_min = (~~phys_min * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_min * (1+(damage_bonus+damage_enhanced)/100)));
+				phys_max = (~~phys_max * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_max * (1+(damage_bonus+damage_enhanced+(character.level*character.e_max_damage_per_level))/100)));
+		}
+//		phys_min = (~~phys_min * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_min * (1+(damage_bonus+damage_enhanced)/100)));
+//		phys_max = (~~phys_max * (phys_mult + damage_bonus/100) * (1 + (weapon_damage-100)/100) + (damage_max * (1+(damage_bonus+damage_enhanced+(character.level*character.e_max_damage_per_level))/100)));
 		if (spell != 2) { skillMin = Math.floor(phys_min+nonPhys_min); skillMax = Math.floor(phys_max+nonPhys_max); }
 		if (spell == 0) { skillAr = Math.floor(ar*(1+ar_bonus/100)); }
 		
